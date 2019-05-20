4e0505ef8b17dde415cdf4ba724f5d1649af3be6
Mert Duman <30327632+MertDuman@users.noreply.github.com>
2019-05-20 20:53:11 +0300

    Company page almost finished (except notifications)

--- company_page.php
+++ company_page.php
@@ -61,6 +61,71 @@
 
         echo json_encode($representatives);
         exit();
+    } else if (isset($_POST["follows"]) && !empty($_POST["follows"])) {
+        $followers = [];
+        $comp_id = $_POST["follows"];
+        $query = "SELECT Name_first_name, Name_second_name, Username
+                  FROM Account NATURAL JOIN Follows
+                  WHERE CompanyID = ?";
+        $stmt = mysqli_stmt_init($sql_conn);
+        mysqli_stmt_prepare($stmt, $query);
+        mysqli_stmt_bind_param($stmt, "i", $comp_id);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                array_push($followers, array($row["Name_first_name"], $row["Name_second_name"], $row["Username"]));
+            }
+        } else {
+            // No followers
+        }
+        
+        echo json_encode($followers);
+        exit();
+    } else if (isset($_POST["joboffer"]) && !empty($_POST["joboffer"])) {
+        $joboffers = [];
+        $comp_id = $_POST["joboffer"];
+        $query = "SELECT Title, Description, Job_Type, Salary, Quota
+                  FROM Post NATURAL JOIN Company NATURAL JOIN Job_Offering
+                  WHERE CompanyID = ?";
+                  
+        $stmt = mysqli_stmt_init($sql_conn);
+        mysqli_stmt_prepare($stmt, $query);
+        mysqli_stmt_bind_param($stmt, "i", $comp_id);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                array_push($joboffers, array($row["Title"], $row["Description"], $row["Job_Type"], $row["Salary"], $row["Quota"]));
+            }
+        } else {
+            // No job offers
+        }
+        
+        echo json_encode($joboffers);
+        exit();
+    } else if (isset($_POST["review"]) && !empty($_POST["review"])) {
+        $reviews = [];
+        $comp_id = $_POST["review"];
+        $query = "SELECT *
+                  FROM Post NATURAL JOIN Review NATURAL JOIN Company
+                  WHERE CompanyID = ?";
+                  
+        $stmt = mysqli_stmt_init($sql_conn);
+        mysqli_stmt_prepare($stmt, $query);
+        mysqli_stmt_bind_param($stmt, "i", $comp_id);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                array_push($reviews, array($row["Anonimity"], $row["Rating"], $row["Pros"], $row["Cons"]));
+            }
+        } else {
+            // No job offers
+        }
+        
+        echo json_encode($reviews);
+        exit();
     }
 ?>
 <!DOCTYPE html>
@@ -129,68 +194,147 @@
         
         var cid = "<?php echo $comp_id ?>";
         
-        function changeContent(element, type) {
-            header = document.getElementById("company_header");
-            info = document.getElementById("company_info");
+        function loadGeneralInfo(element) {
+            var header = document.getElementById("company_info_header");
+            var info = document.getElementById("company_info");
             var url = window.location.href;
             url = url.substring(url.lastIndexOf("/")+1);
+            
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: {geninfo: cid},
+                success: function(res) {
+                    head = res.substring(0, res.indexOf(":"));
+                    header.innerHTML = head;
 
-            if (type == 0) {
-                $.ajax({
-                    type: "POST",
-                    url: url,
-                    data: {geninfo: cid},
-                    success: function(res) {
-                        console.log(res);
-                        head = res.substring(0, res.indexOf(":"));
-                        header.innerHTML = head;
-
-                        inf = res.substring(res.indexOf(":")+1);
-                        info.innerHTML = inf;
-                    }
-                });
-            } else if (type == 1) {
-                
-            } else {
-                $.ajax({
-                    type: "POST",
-                    url: url,
-                    data: {repr: cid},
-                    success: function(res) {
-                        console.log(res);
-                        reps = JSON.parse(res);
-                        
-                        header.innerHTML = "Representatives";
-                           
-                        info.innerHTML = "";
-                        for (rep of reps) {
-                            info.innerHTML += "Full Name: " + rep[0] + " " + rep[1] + ", Phone: " + rep[2] + "<br>";
-                        }
-                        
-                        if (reps.length == 0) {
-                            info.innerHTML = "Sorry, there are no representatives for this company."
-                        }
-                    }
-                });
-            }
+                    inf = res.substring(res.indexOf(":")+1);
+                    info.innerHTML = inf;
+                }
+            });
         }
-
-        function showContactInfo(element) {
-            header = document.getElementById("company_header");
-            info = document.getElementById("company_info");
-
+        
+        function loadFollowers(element) {
+            var followers = document.getElementById("company_followers");
             var url = window.location.href;
             url = url.substring(url.lastIndexOf("/")+1);
+            
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: {follows: cid},
+                success: function(res) {
+                    flws = JSON.parse(res);
+                    
+                    followers.innerHTML = "";
+                    for (flw of flws) {
+                        followers.innerHTML += "Full Name: " + flw[0] + " " + flw[1] + ", Username: " + flw[2] + "<br>";
+                    }
+                    
+                    if (flws.length == 0) {
+                        followers.innerHTML = "Sorry, this company has no followers.";
+                    }
+                }
+            });
+        }
+        
+        function loadRepresentatives(element) {
+            var representatives = document.getElementById("company_representatives");
+            var url = window.location.href;
+            url = url.substring(url.lastIndexOf("/")+1);
+            
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: {repr: cid},
+                success: function(res) {
+                    console.log(res);
+                    reps = JSON.parse(res);
+                       
+                    representatives.innerHTML = "";
+                    for (rep of reps) {
+                        representatives.innerHTML += "Full Name: " + rep[0] + " " + rep[1] + ", Phone: " + rep[2] + "<br>";
+                    }
+                    
+                    if (reps.length == 0) {
+                        representatives.innerHTML = "Sorry, there are no representatives for this company."
+                    }
+                }
+            });
+        }
+        
+        function loadReviews(element) {
+            var reviews = document.getElementById("company_reviews");
+            var url = window.location.href;
+            url = url.substring(url.lastIndexOf("/")+1);
+            
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: {review: cid},
+                success: function(res) {
+                    rews = JSON.parse(res);
+                    
+                    reviews.innerHTML = "";
+                    for (rew of rews) {
+                        if (rew[0] == 1) {
+                            reviews.innerHTML += "<p>Anonymous Post</p>";  
+                        } else {
+                            reviews.innerHTML += "<p>Poster: </p>";  
+                        }
+                        reviews.innerHTML += "<p>Rating: " + rew[1] + "</p>";
+                        reviews.innerHTML += "<p>Pros: " + rew[2] + "</p>";
+                        reviews.innerHTML += "<p>Cons: " + rew[3] + "</p>";
+                    }
+                    
+                    if (rews.length == 0) {
+                        reviews.innerHTML = "Sorry, there are no reviews for this company.";
+                    }
+                }
+            });
+        }
+        
+        function loadJobOfferings(element) {
+            var jobofferings = document.getElementById("company_jobofferings");
+            var url = window.location.href;
+            url = url.substring(url.lastIndexOf("/")+1);
+            
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: {joboffer: cid},
+                success: function(res) {
+                    offers = JSON.parse(res);
+                    
+                    jobofferings.innerHTML = "";
+                    for (offer of offers) {
+                        jobofferings.innerHTML += "<h5>" + offer[0] + "</h5>";
+                        jobofferings.innerHTML += "<p>" + offer[1] + "</p>";
+                        jobofferings.innerHTML += "<p>Job Type: " + offer[2] + "</p>";
+                        jobofferings.innerHTML += "<p>Salary: " + offer[3] + "</p>";
+                        jobofferings.innerHTML += "<p>Quota: " + offer[4] + "</p><br>";
+                    }
+                    
+                    if (offers.length == 0) {
+                        jobofferings.innerHTML = "Sorry, this company has no job offers.";
+                    }
+                }
+            });
+        }
+        
+        function loadContactInfo(element) {
+            var contact = document.getElementById("company_contact");
+            var url = window.location.href;
+            url = url.substring(url.lastIndexOf("/")+1);
+            
             $.ajax({
                 type: "POST",
                 url: url,
                 data: {address: cid},
                 success: function(res) {
-                    info.innerHTML = res;
+                    contact.innerHTML = res;
                 }
             });
-
-            header.innerHTML = "Contact Info";
         }
 
     </script>
@@ -216,7 +360,7 @@
         <div class="container">
             <div class="col-md-6 intro-info order-md-first order-last">
                 <?php
-                    include("dbconfig.php");
+                    $sql_conn = Connect();
                     $comp_id = $_POST["cid"];
                     $comp_name = "";
                     $comp_info = "";
@@ -250,32 +394,115 @@
                     echo "<h2 style='font-size: 48px;font-weight: 700;'>$comp_name</h2>";
                 ?>
             </div>
-
-            <nav style="margin-top: 20px" id="mynav" class="main-nav">
-                <ul>
-                    <li class="nav-item dropdown active">
-                      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                        About
-                      </a>
-                      <div class="dropdown-menu">
-                        <a class="dropdown-item active" href="#" onclick="changeContent(this, 0);">General Information</a>
-                        <a class="dropdown-item" href="#" onclick="changeContent(this, 1);">Followers</a>
-                        <a class="dropdown-item" href="#" onclick="changeContent(this, 2);">Representatives</a>
-                      </div>
-                    </li>
-                    <li><a href="#intro">Notifications</a></li>
-                    <li><a href="#about">Reviews</a></li>
-                    <li><a href="#services">Job Offerings</a></li>
-                    <li><a href="#" onclick="showContactInfo(this)">Contact</a></li>
-                </ul>
-            </nav><!-- .main-nav -->
-
-            <div style="margin-top: 40px;">
-                <div class="jumbotron">
-                    <div class="row">
-                        <div class="col-sm-8">
-                            <?php echo "<h1 id='company_header'>About $comp_name</h1>"; ?>
-                            <p id="company_info"><?php echo $comp_info ?></p>
+            <div class="container">
+                <div class="row">
+                    <div class="nav flex-row nav-pills" id="v-pills-tab" role="tablist" aria-orientation="horizontal">
+                      <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" 
+                      href="#geninfo" onclick="loadGeneralInfo(this);" role="tab" aria-controls="v-pills-home" aria-selected="true">General Information</a>
+                      
+                      <a class="nav-link" id="v-pills-employment-tab" data-toggle="pill" 
+                      href="#followers" onclick="loadFollowers(this);" role="tab" aria-controls="v-pills-employment" aria-selected="false">Followers</a>
+                      
+                      <a class="nav-link" id="v-pills-reviews-tab" data-toggle="pill" 
+                      href="#representatives" onclick="loadRepresentatives(this);" role="tab" aria-controls="v-pills-reviews" aria-selected="false">Representatives</a>
+                      
+                      <a class="nav-link" id="v-pills-applications-tab" data-toggle="pill" 
+                      href="#notifications" role="tab" aria-controls="v-pills-applications" aria-selected="false">Notifications</a>
+                      
+                      <a class="nav-link" id="v-pills-notifications-tab" data-toggle="pill" 
+                      href="#reviews" onclick="loadReviews(this);" role="tab" aria-controls="v-pills-notifications" aria-selected="false">Reviews</a>
+                      
+                      <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" 
+                      href="#jobofferings" onclick="loadJobOfferings(this);" role="tab" aria-controls="v-pills-settings" aria-selected="false">Job Offerings</a>
+                      
+                      <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" 
+                      href="#contact" onclick="loadContactInfo(this);" role="tab" aria-controls="v-pills-settings" aria-selected="false">Contact</a>
+                    </div>
+                </div>
+            
+                <div class="tab-content">
+                    <div class="tab-pane fade container show active" id="geninfo" role="tabpanel">
+                        <div style="margin-top: 40px;">
+                            <div class="jumbotron">
+                                <div class="row">
+                                    <div class="col-sm-8">
+                                        <h1 id="company_info_header"><?php echo "About $comp_name</h1>"; ?></h1>
+                                        <p id="company_info"><?php echo $comp_info ?></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade container" id="followers" role="tabpanel">
+                        <div style="margin-top: 40px;">
+                            <div class="jumbotron">
+                                <div class="row">
+                                    <div class="col-sm-8">
+                                        <h1 id="company_followers_header">Followers</h1>
+                                        <p id="company_followers"></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade container" id="representatives" role="tabpanel">
+                        <div style="margin-top: 40px;">
+                            <div class="jumbotron">
+                                <div class="row">
+                                    <div class="col-sm-8">
+                                        <h1 id="company_representatives_header">Representatives</h1>
+                                        <p id="company_representatives"></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade container" id="notifications" role="tabpanel">
+                        <div style="margin-top: 40px;">
+                            <div class="jumbotron">
+                                <div class="row">
+                                    <div class="col-sm-8">
+                                        <?php echo "<h1 id='company_header'>About $comp_name</h1>"; ?>
+                                        <p id="company_info"><?php echo $comp_info ?></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade container" id="reviews" role="tabpanel">
+                        <div style="margin-top: 40px;">
+                            <div class="jumbotron">
+                                <div class="row">
+                                    <div class="col-sm-8">
+                                        <h1>Reviews</h1>
+                                        <p id="company_reviews"></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade container" id="jobofferings" role="tabpanel">
+                        <div style="margin-top: 40px;">
+                            <div class="jumbotron">
+                                <div class="row">
+                                    <div class="col-sm-8">
+                                        <h1 id="company_jobofferings_header">Job Offerings</h1>
+                                        <p id="company_jobofferings"></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade container" id="contact" role="tabpanel">
+                        <div style="margin-top: 40px;">
+                            <div class="jumbotron">
+                                <div class="row">
+                                    <div class="col-sm-8">
+                                        <h1 id="company_contact_header">Contact Info</h1>
+                                        <p id="company_contact"></p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
